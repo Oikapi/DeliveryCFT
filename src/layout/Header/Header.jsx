6d42fb76b242994fec77cf4dcaffe0c0c523f26e
Oikapi <seryy.gordeev.78@mail.ru>
2024-01-30 23:47:05 +0700

    Initial commit

---
+++ src/layout/Header/Header.jsx
@@ -0,0 +1,14 @@
+
+
+
+const Header = () => {
+
+
+    return (
+        <div>
+            header
+        </div>
+    )
+}
+
+export default Header
---
+++ src/router/index.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [{
+            path: "/",
+            element: <div>HEllo</div>
+        },]
+    }
+])
+
+
+export default router
